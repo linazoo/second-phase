41514675b7e0cd4114c6748babece7d89096a4c1
Nate Delage <nate@natedelage.com>
2014-03-10 09:07:12 -0500

    Remove duplicate students.

--- resources/groups.rb
+++ resources/groups.rb
@@ -1,21 +1,4 @@
-students = ["Adam Willis", "Calder Wishne",
-            "Calder Wishne", "Cameron Johnson",
-            "Cameron Johnson", "Dan Benson",
-            "Dan Benson", "Dan Brown",
-            "Dan Brown", "Devin Wieland",
-            "Devin Wieland", "Dinesh Kaushal",
-            "Dinesh Kaushal", "Greg Santikian",
-            "Greg Santikian", "Ian Bates",
-            "Ian Bates", "Jarrod Scott",
-            "Jarrod Scott", "John Norton",
-            "John Norton", "Kelsey Ando",
-            "Kelsey Ando", "Kenny Luk",
-            "Kenny Luk", "Marco Morales",
-            "Marco Morales", "Osagie Omoruyi",
-            "Osagie Omoruyi", "Pablo Vergel",
-            "Pablo Vergel", "Sarah Sprague",
-            "Sarah Sprague", "Tony Ta",
-            "Tony Ta"]
+students = ["Adam Willis", "Calder Wishne", "Cameron Johnson", "Dan Benson", "Dan Brown", "Devin Wieland", "Dinesh Kaushal", "Greg Santikian", "Ian Bates", "Jarrod Scott", "John Norton", "Kelsey Ando", "Kenny Luk", "Marco Morales", "Pablo Vergel", "Sarah Sprague", "Tony Ta"]
 
 member_count = (ARGV.shift || 4).to_i
 
