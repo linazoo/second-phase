27cf0345c91116035c9c6f692c2345fd5d1888b6
Alyssa Diaz <alydiaz77@gmail.com>
2016-08-30 10:57:52 -0500

    Adds javascript-drills to phase config

--- bin/phase_config.rb
+++ bin/phase_config.rb
@@ -21,6 +21,7 @@ module PhaseConfig
   def challenges_as_list(path)
     list = find_challenges(path).map{|challenge| challenge[2]}.flatten
     list << "sinatra-skeleton-mvc-challenge" #temporary
+    list << "javascript-drills" #temporary
     list.sort
   end
 
